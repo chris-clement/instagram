a68be5faac4dbbcc3e023c806e463bbdb5795a5b
Chris Clement <chrisclement@Chriss-MacBook-Air.local>
2022-01-22 15:28:20 +0000

    Added image uploads to tests to stop them failing

--- instagram/spec/features/viewing_a_full_post_spec.rb
+++ instagram/spec/features/viewing_a_full_post_spec.rb
@@ -5,15 +5,17 @@ RSpec.feature "Viewing a full post", type: :feature do
     visit "/"
     click_link "Create new post"
     fill_in "post[content]", with: "Look at my post over here"
+    attach_file("post[post_photo]", File.join(Rails.root, "/app/assets/images/beach_photo.jpeg" ))
     click_button "Submit"
     visit "/"
     click_link "Full Post"
-    expect(page).to have_content("Image Placeholder")
+    expect(page).to have_content("Look at my post over here")
   end
   scenario "A user makes a post and a timestamp is shown of when it was posted in the full post details" do
     visit "/"
     click_link "Create new post"
     fill_in "post[content]", with: "Look at my post over here"
+    attach_file("post[post_photo]", File.join(Rails.root, "/app/assets/images/beach_photo.jpeg" ))
     click_button "Submit"
     visit "/"
     click_link "Full Post"
